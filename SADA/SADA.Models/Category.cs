94e7845b91bfff560e9941f93b9df33d644e6249
Nada Mahmoud <nada.mhmudd@gmail.com>
2022-01-20 22:51:33 -0800

    1. strucred to N Tier Architecture 2. create Db Connection 3. Create Category table with CRUD operations 4. Add Bootstrap theme

---
+++ SADA/SADA.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SADA.Models;
+
+public class Category
+{
+    [Key] //Primary Key
+    public int Id { get; set; }
+
+    [Required] //can't be null
+    public string Name { get; set; }
+
+    [Display(Name = "Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
+    public int DisplayOrder { get; set; } //can't be null becuse it is int "not nullable type"
+    
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
+
---
+++ SADA/SADAWeb/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SADA.Database;
+using SADA.Models;
+
+namespace SADA.Controllers;
+public class CategoryController : Controller
+{
+    // To achieve dependency injection
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db) => _db = db;
+    public IActionResult Index()
+    {
+        IEnumerable<Category> categoryList = _db.Categories;
+        return View(categoryList);
+    }
+
+
+    //GET
+    public IActionResult Create() => View();
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+
+        if (ModelState.IsValid) //depends on constraints in the model
+        {
+            _db.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+
+            return RedirectToAction("Index");
+        }
+        
+        return View(obj);
+    }
+
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id is null || id == 0)
+            return NotFound();
+
+        var categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb is null) 
+            return NotFound();
+
+        return View(categoryFromDb);
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+
+        if (ModelState.IsValid) //depends on constraints in the model
+        {
+            _db.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+
+            return RedirectToAction("Index");
+        }
+
+        return View(obj);
+    }
+
+
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id is null || id == 0)
+            return NotFound();
+
+        var categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb is null)
+            return NotFound();
+
+        return View(categoryFromDb);
+    }
+    //POST
+    [HttpPost/*,ActionName("Delete")*/]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(Category obj)
+    {
+        _db.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+
+        return RedirectToAction("Index");
+    }
+}
+
