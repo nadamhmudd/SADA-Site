846d343e92e37beade58c7cb2df97ba7cb284586
Nada Mahmoud <nada.mhmudd@gmail.com>
2022-01-21 23:49:25 -0800

    fixeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed

---
+++ SADA.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SADA.Core.Models;
+
+namespace SADA.Database;
+public class ApplicationDbContext : DbContext
+{
+    //General setup that will confiqure DB Context
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    //Create Tables
+    public DbSet<Category> Categories { get; set; }
+}
+
